f80ecbb6846d16464c91bf456af6797bb1f08323
vansh1254 <143412160+vansh1254@users.noreply.github.com>
2023-08-29 17:13:41 +0530

    Update info.py

--- info.py
+++ info.py
@@ -3,6 +3,6 @@ API_HASH     = "812529f879f06436925c7d62eb49f5d1"
 BOT_TOKEN    = "6590082039:AAGWEMkuphepkyIrECQFGMTBAJ5VKXaBFMM"
 SESSION      = "BQFPB4gAfwfbeM3IBRoZOEqosM2OWBUWE8ZZEBTb7gSesguBJozRY6wEdZdhPhhIsbXjflIdaVjXhpsy_tICyrCAAmxqT0tLRXfhMKI4CCeJP7bxTi6Vxo-_X5lHnADbhXhdVbo3zfVqBwHhbpMuCJBrOIzSrZ_Bx9E08Ekldg4fcEp3_uB-Ozjbfc_fP2POxXdBQRWVgAzy9PimWv436sc0KoScptWtnzVpwIa-wBAvnoneffsMhlqwVdCCLwmU-xeINIL2ZyuY5mgaP_K8AyfsAW_gCFs3iRfE8imNChBJmUBtCaf9K1ACYvudhvxpcQ0SNTyySlkPtjgMDC02jremq3-HFQAAAAB4ai0IAA"
 DATABASE_URI = "mongodb+srv://smmpanelup:9719619584@cluster0.mwjxbie.mongodb.net/?retryWrites=true&w=majority"
-LOG_CHANNEL  = -1001904265866
+LOG_CHANNEL  = -1001870213031
 ADMIN        = 2020224264
 CHANNEL      = "@moviekorner_1"
